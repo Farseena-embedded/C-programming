f587200af282cb8061cda398317170dcbc193e4d
Farseena CK <farseenack515@gmail.com>
2026-02-08 20:39:23 +0530

    Add data type program

---
+++ Data_type/Data_type_basic.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int a =20;
+    float b =2.05;
+    char c='A';
+    double val=25.45618;
+
+    printf("a=%d\n",a);
+    printf("b=%.2f\n",b);
+    printf("c=%c\n",c);
+    printf("val=%f",val);
+    return 0;
+}
---
+++ Data_type/size.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+   float val1 = 3.14;
+   int val2 = 20; 
+   char val3 = 'A';
+   printf("Size of float: %d\n",sizeof(float));
+   printf("Size of integer: %d\n", sizeof(int));
+   printf("Size of charactor:%d\n",sizeof(char));
+   return 0;
+}
